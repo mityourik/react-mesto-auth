4f2d840ac5a211b5086a4f376c83a18d227f5f5d
Dmitry <mityourik@gmail.com>
2023-10-25 23:39:28 -0300

    add api before url api

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -96,7 +96,7 @@ export class Api {
 
 //класс для апи
 export const api = new Api({
-  url: 'https://mityourik.nomoredomainsrocks.ru/',
+  url: 'https://api.mityourik.nomoredomainsrocks.ru/',
   headers: {
     'Content-Type': 'application/json',
     authorization: 'a2b723e3-a104-4268-8462-81c1140190b0'
